c9163807dc00752579a1e07fe143d7700a64a33f
Nicola Pisani <pisani_nicola@yahoo.com>
2023-07-14 15:01:26 +0200

    component-lifecycle-componentDidMount

--- hello/src/App.js
+++ hello/src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>Welcome to My App</h1>
+        <h1>Welcome to My App's</h1>
         <Counter/>
       </div>
     );
