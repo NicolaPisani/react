65dba9f52aa7555d6e182240895d943ec502cbf9
Nicola Pisani <pisani_nicola@yahoo.com>
2023-07-29 16:36:20 +0200

    modify GitHubUser swr parte 3

--- hello/src/useGithubUser.js
+++ hello/src/useGithubUser.js
@@ -1,67 +1,20 @@
-import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 export const useGithubUser = (username) => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { data, error, mutate } = useSWR(
+    username ? `https://api.github.com/users/${username}` : null
+  );
 
   const fetchDataGit = async () => {
-    setLoading(true);
-    setError(null);
     try {
-      if (!username) {
-        setData(null); // Imposta i dati a null se il nome utente è null
-      } else {
-        const response = await fetch(
-          `https://api.github.com/users/${username}`
-        );
-        const data = await response.json();
-        setData(data);
-      }
+      const response = await fetch(`https://api.github.com/users/${username}`);
+      const data = await response.json();
+      mutate(data, false); // Update the data without revalidation
     } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
+      console.error("Error fetching user data:", error);
     }
   };
 
-  useEffect(() => {
-    fetchDataGit();
-  }, [username]);
-
-  return [fetchDataGit, data, error, loading];
+  return { userData: data, loading: !data && !error, error, fetchDataGit };
 };
 
-export default useGithubUser;
-
-// import { useState } from "react";
-
-// export const useGithubUser = ({ username }) => {
-//   const [data, setData] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(null);
-
-//   const fetchDataGit = async (username) => {
-//     setLoading(true);
-//     setError(null);
-//     try {
-//       const response = await fetch(`https://api.github.com/users/${username}`);
-
-//       const data = await response.json();
-
-//       if (data.status === 404) {
-//         throw new Error("error");
-//       } else if (data.status !== 200) {
-//         throw new Error("error");
-//       } else {
-//         setData(data);
-//       }
-//     } catch (error) {
-//       setError(error);
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return [fetchDataGit, data, error, loading];
-// };
